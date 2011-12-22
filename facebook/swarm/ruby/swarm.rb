7320e3dff505530758ba3bca2b6487682bfdc8d7
Xiang Xiao <xxiao23@gmail.com>
2011-12-22 12:27:47 -0800

    finished implementing DP knapsack solution for this problem; passed facebook supplied test case

--- facebook/swarm/ruby/swarm.rb
+++ facebook/swarm/ruby/swarm.rb
@@ -7,7 +7,94 @@
 #      sub routines     #
 #########################
 def optimal_attack_plan_for_planet planet_id
+  # number of bases on this planet
+  num_bases = $num_bases_per_planet[planet_id]
+  # total zerg force available
+  total_z_force = $z_force_per_planet[planet_id]
+  # max_mineral matrix
+  # first row and column are all 0s 
+  mm = Array.new
+  # constrcut a max_mineral solution
+  # first row and column are all 0s 
+  mc = Array.new
+  for k in (0..num_bases)
+    mm.push Array.new(total_z_force+1, 0)
+    mc.push Array.new(total_z_force+1, 0)
+  end
+  puts "#{mm.inspect}" if $debug
+  puts "#{mc.inspect}" if $debug
+  # compute mm from bottom up
+  # mm[k][i] = max { mm[k-1][i-z[k]]+m[k], mm[k-1][i] }
+  for k in (1..num_bases)
+    for i in (1..total_z_force)
+      prev_i = i - $min_z_force_per_base[planet_id][k]
+      if prev_i < 0 then
+        # zerg cannot take any minerals
+        # if not enough zerg force
+        current_max = 0
+      else
+        current_max = mm[k-1][prev_i] + $m_per_base[planet_id][k]
+      end
+      # in this case
+      # we have picked base k
+      mc[k][i] = 1
+      if current_max<=mm[k-1][i] then
+        # in this case
+        # we don't pick base k
+        # a subtlety here
+        # if attacking base k dones't get us any more mineral
+        # then don't attack
+        # in this way, we can handle the case
+        # where current_max and mm[k-1][i] are both 0s
+        # which means we don't have enough zerg froce 
+        # to take over any of the bases considered
+        current_max = mm[k-1][i]
+        mc[k][i] = 0
+      end
+      mm[k][i] = current_max
+    end
+  end
   
+  if $debug then
+    puts "max mineral matrix"
+    for i in (1..num_bases)
+      puts "base #{i}: #{mm[i].inspect}"
+    end
+    puts "max mineral construction matrix"
+    for i in (1..num_bases)
+      puts "base #{i} : #{mc[i].inspect}"
+    end
+  end
+  
+  # construct the optimal zerg force deployment
+  # from matrix mc
+  k = num_bases
+  i = total_z_force
+  total_z_force_deployed = 0
+  total_mineral_captured = 0
+  # base_id => zerg force deployed
+  bases_attacked = Hash.new
+  while k > 0 and i > 0
+    if mc[k][i] == 1 then
+      # base k is attacked
+      total_z_force_deployed += $min_z_force_per_base[planet_id][k]
+      total_mineral_captured += $m_per_base[planet_id][k]
+      bases_attacked[k] = $min_z_force_per_base[planet_id][k]
+      i -= $min_z_force_per_base[planet_id][k]
+    end
+    k -= 1
+  end
+  puts "total force deployed on planet #{planet_id} : #{total_z_force_deployed}" if $debug
+  puts "deployment plan : #{bases_attacked.inspect}" if $debug
+  
+  # print out the solution
+  # in a required form
+  puts "#{total_z_force_deployed} #{total_mineral_captured}"
+  tmp_str = ""
+  bases_attacked.keys.sort.each { |k|
+    tmp_str += "#{k-1} #{bases_attacked[k]} "
+  }
+  puts "#{tmp_str.chomp()}"
 end
 
 #########################
@@ -86,6 +173,12 @@ while line = input_file.gets
     $num_bases_per_planet[planet_id] = tmp[0].to_i
     $z_force_per_planet[planet_id] = tmp[1].to_i
     num_bases_line = false
+    # create a dummy base 0
+    # this makes DP convinient
+    $t_force_per_base[planet_id].push 0
+    $m_per_base[planet_id].push 0
+    $min_z_force_per_base[planet_id].push 0
+    base_id += 1
     next
   end
   # now this is a line for a specific base: t_force mineral
@@ -98,7 +191,7 @@ while line = input_file.gets
   puts "mineral #{$m_per_base.inspect}" if $debug
   # start from next line are for next planet
   base_id += 1 
-  if base_id == $num_bases_per_planet[planet_id] then
+  if base_id > $num_bases_per_planet[planet_id] then
     num_bases_line = true
     planet_id += 1
     base_id = 0
@@ -117,9 +210,12 @@ end
 #########################
 #       main process    #
 #########################
+for i in (0..($num_planets-1)) 
+  optimal_attack_plan_for_planet i
+end
 
 #########################
 #       validation      #
 #########################
-return if !$debug
+exit if !$debug
 
