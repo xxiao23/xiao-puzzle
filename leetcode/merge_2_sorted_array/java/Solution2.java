db93b91dd018c17ad74a919ac6f29d64828d3fc1
xxiao23 <xxiao23@gmail.com>
2013-11-20 16:51:35 -0800

    merge from the tail, backwards. in place with O(n)

---
+++ leetcode/merge_2_sorted_array/java/Solution2.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public void merge(int A[], int m, int B[], int n) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        
+        // merge from the tail
+        int i = m-1, j = n-1, k = m+n-1;
+        while (i >= 0 && j >= 0) {
+            if (A[i] > B[j]) {
+                A[k--] = A[i--];
+            } else {
+                A[k--] = B[j--];
+            }
+        }
+        while (j >= 0) {
+            A[k--] = B[j--];
+        }
+    }
+}
